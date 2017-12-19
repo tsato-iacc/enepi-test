20c52a823804068c73d6448d915b1d014dfdc1ee
Zazimko Alexey <notfoundsam@gmail.com>
2017-12-19 18:53:42 +0900

    fix header

--- fuel/app/views/front/header.php
+++ fuel/app/views/front/header.php
@@ -1,53 +1,26 @@
+
 <header>
   <div class="container-wrap">
     <div class="logo-wrap pos-left">
       <div class="logo">
-        <div class="container" style="max-width: 1296px;">
-          <?= "<a ".MyView::link_to("/", ["class" => "logo logo-position"]).">".Asset::img('layout/logo.png')."</a>"; ?>
-        </div>
+        <div><a href="<?= \Uri::base(); ?>"><?= Asset::img('layout/logo.png'); ?></a></div>
       </div>
     </div>
-
-
-
-
-
     <div class="navi-wrap">
-
-      <?
-         $header_link = [
-             'simple_simulations'   => ['uri' => '/simple_simulations/new',   'name' => 'ガス料金<br>シミュレーション'],
-             'local_contents'       => ['uri' => '/local_contents',           'name' => '地域別<br>ガス料金検索'],
-             'lpgas'                => ['uri' => '/categories/lpgas',         'name' => 'プロパンガス<br>(LPガス)記事一覧']
-         ];
-
-         foreach($header_link as $hl => $h){ ?>
-         <div class="navi">
-
-        <? if($_SERVER['REQUEST_URI'] == $h['uri']){
-             $hash = ['class' => 'active']?>
-             <?= '<a '.MyView::link_to($h['uri'], $hash).'>'.$h['name'].'</a>'?>
-        <? }else{ ?>
-             <?= MyView::link_to($h['name'], $h['uri'])?>
-        <? } ?>
-
-         </div>
-      <? }?>
+      <div class="navi">
+        <a href="/simple_simulations/new" class="<?= \Uri::segment(1) == 'simple_simulations' ? ' active' : ''?>">ガス料金<br>シミュレーション</a>
+      </div>
+      <div class="navi">
+        <a href="/local_contents" class="<?= \Uri::segment(1) == 'local_contents' ? ' active' : ''?>">地域別<br>ガス料金検索</a>
+      </div>
+      <div class="navi">
+        <a href="/categories/lpgas" class="<?= \Uri::segment(1) == 'categories' && \Uri::segment(2) == 'lpgas' ? ' active' : ''?>">プロパンガス<br>(LPガス)記事一覧</a>
+      </div>
     </div>
-
-
-
-
-
-
-<!-- <div class="logo-wrap pos-right">
+    <div class="logo-wrap pos-right">
       <div class="enepi-tel">
--->
-    <span class="tel-link-img">
-        <div><?= Asset::img('layout/enepi_tel.png', array('class' => 'tel', 'alt' => 'Img tel'));?></div>
-    </span>
-<!-- </div>
+        <div><?= Asset::img('layout/enepi_tel.png', ['alt' => 'Enepi']); ?></div>
+      </div>
     </div>
- -->
   </div>
 </header>
