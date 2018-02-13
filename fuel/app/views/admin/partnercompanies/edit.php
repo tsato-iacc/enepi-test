b3795d06346556daf43e4e8d6eeef5b4531d7841
Zazimko Alexey <notfoundsam@gmail.com>
2018-02-13 12:00:14 +0900

    fix2

--- fuel/app/views/admin/partnercompanies/edit.php
+++ fuel/app/views/admin/partnercompanies/edit.php
@@ -6,6 +6,6 @@
 
 <?= \Form::open(['action' => \Uri::create('admin/partner_companies/:id', ['id' => $partner_company->id])]); ?>
   <?= \Form::csrf(); ?>
-  <?= render('admin/partnerCompanies/_form', ['val' => $val, 'partner_company' => $partner_company]); ?>
+  <?= render('admin/partnercompanies/_form', ['val' => $val, 'partner_company' => $partner_company]); ?>
   <button type="submit" class="btn btn-primary">更新する</button>
 <?= Form::close(); ?>
