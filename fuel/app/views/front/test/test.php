6b93a36022e4ab60b5783e8a081919fde30fdd06
dfukushi <dfukushi@iacc130>
2018-02-16 13:08:28 +0900

    SMS実装

---
+++ fuel/app/views/front/test/test.php
@@ -0,0 +1,92 @@
+<style>
+div.main{
+	margin: 5% 2%;
+}
+</style>
+
+
+<div class="main">
+
+メール送信テスト<br>
+宛先：<input type="text" value="" placeholder="test@test" id="to_addr">
+<input type="button" value="メール送信" onclick="go_mail()">
+
+<br><br>
+
+
+SMS送信テスト<br>
+宛先番号：<input type="text" value="" placeholder="08012345678" id="sms_num">
+<input type="button" value="SMS送信" onclick="go_sms()">
+
+<br><br>
+
+
+</div>
+
+<script>
+function go_mail(){
+
+	document.body.style.cursor = 'wait';
+
+	to = $("#to_addr")[0].value;
+	if(!to){
+		alert("アドレス入力してください");
+		return;
+	}
+
+
+	$.ajax({
+		  url: 'test',
+		  type: 'POST',
+		  cache: false,
+		  dataType: 'html',
+		  data: {
+			  "cmd" : "mail",
+			  "to" : to
+			 }
+		})
+		.done(function(data, textStatus, jqXHR){
+
+			alert("送信完了");
+
+		})
+		.always(function(data, textStatus, jqXHR){
+			document.body.style.cursor = 'auto';
+		});
+
+}
+
+function go_sms(){
+
+
+	document.body.style.cursor = 'wait';
+
+	to = $("#sms_num")[0].value;
+	if(!to){
+		alert("電話番号を入力してください");
+		return;
+	}
+
+
+	$.ajax({
+		  url: 'test',
+		  type: 'POST',
+		  cache: false,
+		  dataType: 'html',
+		  data: {
+			  "cmd" : "twilio",
+			  "to" : to
+			 }
+		})
+		.done(function(data, textStatus, jqXHR){
+
+			alert("SMS送信完了");
+
+		})
+		.always(function(data, textStatus, jqXHR){
+			document.body.style.cursor = 'auto';
+		});
+
+}
+
+</script>
--- fuel/packages/helper/classes/mail.php
+++ fuel/packages/helper/classes/mail.php
@@ -12,83 +12,28 @@ namespace Helper;
 
 class Mail
 {
-	public static function mail_send_o($e)
+
+	public static function mail_send($from, $to, $subject, $body)
 	{
-
-		$client = \Aws\Ses\SesClient::factory(array(
-				'version'=> 'latest',
-				'region' => "us-east-1",
-				'key'    => 'AKIAI2NFTZUSEGDK5E4Q',
-				'secret' => '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',
-
-
-    			// 環境変数にしないと
-    			'credentials' => [
-    				'key'    => 'AKIAI2NFTZUSEGDK5E4Q',
-    				'secret' => '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',
-    			],
-		));
-
-		try {
-			$result = $client->sendEmail([
-					'Destination' => [
-							'ToAddresses' => [
-									"dfukushi@iacc.co.jp",
-							],
-					],
-					'Message' => [
-							'Body' => [
-									'Html' => [
-											'Charset' => "UTF-8",
-											'Data' => "ああああああ",
-									],
-									'Text' => [
-											'Charset' => "UTF-8",
-											'Data' => "あああああ",
-									],
-							],
-							'Subject' => [
-									'Charset' => "UTF-8",
-									'Data' => "タイトル",
-							],
-					],
-					'Source' => "info@enepi.jp",
-					// If you are not using a configuration set, comment or delete the
-					// following line
-					'ConfigurationSetName' => "ConfigSet",
-			]);
-			$messageId = $result->get('MessageId');
-			echo("Email sent! Message ID: $messageId"."\n");
-
-		} catch (SesException $error) {
-			echo("The email was not sent. Error message: ".$error->getAwsErrorMessage()."\n");
-		}
-	}
-
-
-	public static function mail_send($e)
-	{
-
-		define('REGION','us-east-1');
 
 
 		$client = \Aws\Ses\SesClient::factory(array(
 
-				'credentials' => [
-						'key'    => 'AKIAI2NFTZUSEGDK5E4Q',
-						'secret' => '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',
-				],
+			'credentials' => [
+				'key'    => getenv("AWS_ACCESS_KEY_ID"),
+				'secret' => getenv("AWS_SECRET_ACCESS_KEY"),
+			],
 
-				'version'=> 'latest',
-				'region' => REGION
+			'version'=> "latest",
+			'region' => "us-east-1"
 		));
 
 
 		$request = array();
-		$request['Source'] = "info@enepi.jp";
-		$request['Destination']['ToAddresses'] = array("dfukushi@iacc.co.jp");
-		$request['Message']['Subject']['Data'] = "サブジェクト";
-		$request['Message']['Body']['Text']['Data'] = "ボディ";
+		$request['Source'] = $from;
+		$request['Destination']['ToAddresses'] = array($to);
+		$request['Message']['Subject']['Data'] = $subject;
+		$request['Message']['Body']['Text']['Data'] = $body;
 
 		try {
 			$result = $client->sendEmail($request);
@@ -99,6 +44,34 @@ class Mail
 			//echo("The email was not sent. Error message: ");
 			//echo($e->getMessage()."\n");
 		}
+
+	}
+
+
+
+	public static function sms_send($to, $msg = "")
+	{
+
+		$client = new \Twilio\Rest\Client(
+							getenv("TWILLIO_ACCOUNT_SID"),
+							getenv("TWILLIO_AUTH_TOKEN")
+						);
+
+		// 080-1234-5678 → 818012345678
+		$to = str_replace("-", "", $to);
+		$to = "+81".substr($to, 1);
+		if(strlen($to) != 13){
+			return;
+		}
+
+		$client->messages->create(
+				$to,
+				array(
+					'from' => getenv("TWILLIO_SMS_FROM"),
+					'body' => $msg
+				)
+		);
+
 
 	}
 
