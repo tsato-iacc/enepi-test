f0d06ea509548333f90c45a506f1219ce7ca22ed
root <root@ip-172-31-29-222.ap-northeast-1.compute.internal>
2018-02-18 12:43:12 +0900

    init shell

--- shell/init.sh
+++ shell/init.sh
@@ -1,8 +1,10 @@
-#!/bin/sh
+#!/bin/sh -x
 
 cd `dirname $0`
 
+date
 echo "start"
+pwd
 
 sts=1
 if [ -f ~/.ssh/config ]
@@ -18,12 +20,18 @@ then
 fi
 
 
-for i in `ps -aef | grep ssh | grep ec2-user | grep ":3306" | grep -v grep`
+for p in `ps -aef | grep ssh | grep ec2-user | grep ":3306" | grep -v grep | awk '{print $2}'`
 do
-	p=`echo ${i} | awk '{print $2}'`
 	kill -9 ${p}
 done
 
-ssh -i /var/app/current/shell/pps-prod.pem -f -N -C -g -L *:3309:stg-common.cebp6jbs7ymd.ap-northeast-1.rds.amazonaws.com:3306 ec2-user@54.250.242.33
-ssh -i /var/app/current/shell/pps-prod.pem -f -N -C -g -L *:3308:enepi-prod.cebp6jbs7ymd.ap-northeast-1.rds.amazonaws.com:3306 ec2-user@13.230.197.119
+ssh -i `pwd`/pps-prod.pem -f -N -C -g -L *:3309:stg-common.cebp6jbs7ymd.ap-northeast-1.rds.amazonaws.com:3306 ec2-user@54.250.242.33
+ssh -i `pwd`/pps-prod.pem -f -N -C -g -L *:3308:enepi-prod.cebp6jbs7ymd.ap-northeast-1.rds.amazonaws.com:3306 ec2-user@13.230.197.119
 
+
+sudo cp /etc/localtime /etc/localtime.org
+sudo ln -sf  /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+
+date
+echo "fin!"
+echo
