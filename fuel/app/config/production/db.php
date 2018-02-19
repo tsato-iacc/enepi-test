e1652eecf1544e6ae2008d2b3a88714be40e0527
root <root@ip-172-31-29-222.ap-northeast-1.compute.internal>
2018-02-19 17:23:13 +0900

    本番DBハードコーディング

--- fuel/app/config/production/db.php
+++ fuel/app/config/production/db.php
@@ -1,8 +1,19 @@
 <?php
 /**
- * The production database settings. These get merged with the global settings.
+ * Use this file to override global defaults.
+ *
+ * See the individual environment DB configs for specific config information.
  */
 
 return array(
-
+    'default' => array(
+        'type'        => 'mysqli',
+        'connection'  => array(
+            'hostname'   => getenv('RDS_HOSTNAME'),
+            'port'       => getenv('RDS_PORT'),
+            'database'   => getenv('RDS_DB_NAME'),
+            'username'   => getenv('RDS_USERNAME'),
+            'password'   => 'mi!8e$270e',
+        )
+    ),
 );
