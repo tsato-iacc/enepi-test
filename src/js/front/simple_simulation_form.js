efa187d9d7c10be1d59a42f131dff34bdbca37ed
Zazimko Alexey <notfoundsam@gmail.com>
2018-03-06 10:05:04 +0900

    Fix js

--- src/js/front/simple_simulation_form.js
+++ src/js/front/simple_simulation_form.js
@@ -2,6 +2,19 @@
  * Simple simulation new [simple_simulations/new]
  */
 if ($('form#simple_simulation').length) {
+  /*
+   * SEND FORM
+   */
+  var sendForm = function (el) {
+    var form = el.closest('form');
+
+    if (sending_form !== true) {
+      sending_form = true;
+      ga('send', 'event', 'simulation-execute', 'btn-click', '', 0);
+      form.submit();
+    }
+  }
+
   var sending_form = false;
 
   /*
@@ -46,81 +59,69 @@ if ($('form#simple_simulation').length) {
 
     get_cities_by_prefecture_code(prefecture_code);
   });
-}
 
-/*
- * API Get cities list
- */
-function get_cities_by_prefecture_code(prefecture_code) {
-  var city_code_select = $('#simple_simulation select[name=city_code]');
 
-  if (!prefecture_code) {
-    $('#simple_simulation select[name=city_code]').prop('disabled', true);
-    city_code_select.empty();
-    city_code_select.append($('<option>').html("選択してください").val(''));
+  /*
+   * API Get cities list
+   */
+  function get_cities_by_prefecture_code(prefecture_code) {
+    var city_code_select = $('#simple_simulation select[name=city_code]');
 
-    return;
-  }
+    if (!prefecture_code) {
+      $('#simple_simulation select[name=city_code]').prop('disabled', true);
+      city_code_select.empty();
+      city_code_select.append($('<option>').html("選択してください").val(''));
 
-  $.ajax({
-    url: '/front/api/v1/simulation/cities',
-    type: 'GET',
-    data: {
-      // csrf_token_key: "<?= \Security::fetch_token();?>",
-      prefecture_code: prefecture_code
-    },
-    success: function (data) {
-      if (data.errors) {
-        console.log(data.errors);
-        alert('Error');
+      return;
+    }
+
+    $.ajax({
+      url: '/front/api/v1/simulation/cities',
+      type: 'GET',
+      data: {
+        // csrf_token_key: "<?= \Security::fetch_token();?>",
+        prefecture_code: prefecture_code
+      },
+      success: function (data) {
+        if (data.errors) {
+          console.log(data.errors);
+          alert('Error');
+        }
+        else if (data.result) {
+          city_code_select.empty();
+          city_code_select.append($('<option>').html("選択してください").val(''));
+          
+          $.each(data.result.cities, function(k, v) {
+            city_code_select.append($('<option>').html(v).val(k));
+          });
+
+          $('#simple_simulation select[name=city_code]').prop('disabled', false);
+        }
+      },
+      error: function() {
+        alert('An error has occurred!');
       }
-      else if (data.result) {
-        city_code_select.empty();
-        city_code_select.append($('<option>').html("選択してください").val(''));
-        
-        $.each(data.result.cities, function(k, v) {
-          city_code_select.append($('<option>').html(v).val(k));
-        });
+    });
+  }
 
-        $('#simple_simulation select[name=city_code]').prop('disabled', false);
+  function isNumberValid(name) {
+      var numInput = $('input[name=' + name + ']');
+      var num = numInput.val();
+      var pattern = /^(\d{1,10})$/;
+
+      if (num) {
+        num = convertNumber(num);
+        numInput.val(num);
       }
-    },
-    error: function() {
-      alert('An error has occurred!');
+
+      if (num && num.match(pattern)) {
+        return true;
+      }
+
+      return false;
     }
-  });
-}
 
-/*
- * SEND FORM
- */
-function sendForm(el) {
-  var form = el.closest('form');
-
-  if (sending_form !== true) {
-    sending_form = true;
-    ga('send', 'event', 'simulation-execute', 'btn-click', '', 0);
-    form.submit();
+  function convertNumber(str) {
+    return str.replace(/ー|－|−|-/g, '').replace(/[０-９]/g,function(s){return String.fromCharCode(s.charCodeAt(0)-0xFEE0);});
   }
-}
-
-function isNumberValid(name) {
-    var numInput = $('input[name=' + name + ']');
-    var num = numInput.val();
-    var pattern = /^(\d{1,10})$/;
-
-    if (num) {
-      num = convertNumber(num);
-      numInput.val(num);
-    }
-
-    if (num && num.match(pattern)) {
-      return true;
-    }
-
-    return false;
-  }
-
-function convertNumber(str) {
-  return str.replace(/ー|－|−|-/g, '').replace(/[０-９]/g,function(s){return String.fromCharCode(s.charCodeAt(0)-0xFEE0);});
 }
--- src/js/front/simulation_estimate_form.js
+++ src/js/front/simulation_estimate_form.js
@@ -1,239 +1,240 @@
+if ($('#simulation_register_form').length) {  
+  /*
+   * SEND FORM
+   */
+  var sendForm = function (el) {
+    var form = el.closest('form');
+    var estimateKind = $('input[name=lpgas_contact\\[estimate_kind\\]]:checked').val();
 
-var sending_form = false;
-var form_hidden = true;
+    var zip = $('input[name=zip]').val();
+    var pref = $('input[name=pref]').val();
+    var addr = $('input[name=addr]').val();
 
-/*
- * Check Contact
- */
-$('#contact_btn').on('click', function() {
+    if (estimateKind == 'new_contract') {
+      var zipNewHidden = form.find('input[name=lpgas_contact\\[new_zip_code\\]]');
+      var prefNewHidden = form.find('input[name=lpgas_contact\\[new_prefecture_code\\]]');
+      var addrNewHidden = form.find('input[name=lpgas_contact\\[new_address\\]]');
 
-  if (form_hidden)
-    return false;
+      zipNewHidden.val(zip);
+      prefNewHidden.val(pref);
+      addrNewHidden.val(addr);
+    } else {
+      var zipHidden = form.find('input[name=lpgas_contact\\[zip_code\\]]');
+      var prefHidden = form.find('input[name=lpgas_contact\\[prefecture_code\\]]');
+      var addrHidden = form.find('input[name=lpgas_contact\\[address\\]]');
 
-  var error = false;
-  var error_msg = [];
+      zipHidden.val(zip);
+      prefHidden.val(pref);
+      addrHidden.val(addr);
+    }
 
-  var estimateKind = $('input[name=lpgas_contact\\[estimate_kind\\]]:checked').val();
-  var name = isInputValid('lpgas_contact\\[name\\]');
-  var tel = isTelValid('lpgas_contact\\[tel\\]');
-  var mail = isEmailValid('lpgas_contact\\[email\\]');
-
-  if (!estimateKind) {
-    error = true;
-    error_msg.push('※どちらかを選択してください');
-  }
-
-  if (estimateKind && estimateKind == 'change_contract') {
-    var gasCompany = isInputValid('lpgas_contact\\[gas_contracted_shop_name\\]');
-
-    if (!gasCompany) {
-      error = true;
-      error_msg.push('※ガス会社名を入力してください');
+    $('#contact_btn p').text('送信中...');
+    if (sending_form !== true) {
+      sending_form = true;
+      ga('send', 'event', 'simulation-cv', 'btn-click', '', 0);
+      form.submit();
     }
   }
 
-  if (!name) {
-    error = true;
-    error_msg.push('※お名前を入力してください');
-  }
+  var sending_form = false;
+  var form_hidden = true;
 
-  if (!tel) {
-    error = true;
-    error_msg.push('※電話番号を入力してください');
-  }
+  /*
+   * Check Contact
+   */
+  $('#contact_btn').on('click', function() {
 
-  if (!mail) {
-    error = true;
-    error_msg.push('※メールアドレスを入力してください');
-  }
+    if (form_hidden)
+      return false;
 
-  if (error === false) {
+    var error = false;
+    var error_msg = [];
+
+    var estimateKind = $('input[name=lpgas_contact\\[estimate_kind\\]]:checked').val();
+    var name = isInputValid('lpgas_contact\\[name\\]');
+    var tel = isTelValid('lpgas_contact\\[tel\\]');
+    var mail = isEmailValid('lpgas_contact\\[email\\]');
+
+    if (!estimateKind) {
+      error = true;
+      error_msg.push('※どちらかを選択してください');
+    }
+
+    if (estimateKind && estimateKind == 'change_contract') {
+      var gasCompany = isInputValid('lpgas_contact\\[gas_contracted_shop_name\\]');
+
+      if (!gasCompany) {
+        error = true;
+        error_msg.push('※ガス会社名を入力してください');
+      }
+    }
+
+    if (!name) {
+      error = true;
+      error_msg.push('※お名前を入力してください');
+    }
+
+    if (!tel) {
+      error = true;
+      error_msg.push('※電話番号を入力してください');
+    }
+
+    if (!mail) {
+      error = true;
+      error_msg.push('※メールアドレスを入力してください');
+    }
+
+    if (error === false) {
+      isError(false, $(this));
+      // POST FORM
+      sendForm($(this));
+    } else {
+      isError(true, $(this), error_msg[0]);
+    }
+  });
+
+  /**
+   * Remove errors on click
+   */
+  $('.remove-error label').on('click', function() {
+    $(this).closest('form').find('.step-error').removeClass('show-error');
+  });
+  $('input[type=text], input[type=tel]').on('click', function() {
+    $(this).closest('.error-wrap').removeClass('input-error');
     isError(false, $(this));
-    // POST FORM
-    sendForm($(this));
-  } else {
-    isError(true, $(this), error_msg[0]);
-  }
-});
+  });
+  $('select').on('click', function() {
+    $(this).closest('.error-wrap').removeClass('input-error');
+    isError(false, $(this));
+  });
 
-/**
- * Remove errors on click
- */
-$('.remove-error label').on('click', function() {
-  $(this).closest('form').find('.step-error').removeClass('show-error');
-});
-$('input[type=text], input[type=tel]').on('click', function() {
-  $(this).closest('.error-wrap').removeClass('input-error');
-  isError(false, $(this));
-});
-$('select').on('click', function() {
-  $(this).closest('.error-wrap').removeClass('input-error');
-  isError(false, $(this));
-});
+  /**
+   * Show or hide optional label
+   */
+  $('.can-hide').hide();
 
-/**
- * Show or hide optional label
- */
-$('.can-hide').hide();
+  $('input[name=lpgas_contact\\[estimate_kind\\]]').on('click', function() {
+    if (form_hidden) {
+      $('.hidden-field').removeClass('hidden-field');
+      $('.simulation-page-button').removeClass('color-gray');
+      $('.simulation-page-button p').text('WEBで料金プランを見る');
+      $('.simulation-page-button .free-img').removeClass('img-hide');
+      form_hidden = false;
+    }
 
-$('input[name=lpgas_contact\\[estimate_kind\\]]').on('click', function() {
-  if (form_hidden) {
-    $('.hidden-field').removeClass('hidden-field');
-    $('.simulation-page-button').removeClass('color-gray');
-    $('.simulation-page-button p').text('WEBで料金プランを見る');
-    $('.simulation-page-button .free-img').removeClass('img-hide');
-    form_hidden = false;
-  }
+    var estimate = $(this).val();
+    if ($(this).val() == 'new_contract') {
+      $('.can-hide').show();
+      inputHideError('lpgas_contact\\[gas_contracted_shop_name\\]');
+      ga('send', 'event', 'simulation-new-contract', 'btn-click', '', 0);
+    } else {
+      $('.can-hide').hide();
+      ga('send', 'event', 'simulation-change-contract', 'btn-click', '', 0);
+    }
+  });
 
-  var estimate = $(this).val();
-  if ($(this).val() == 'new_contract') {
-    $('.can-hide').show();
-    inputHideError('lpgas_contact\\[gas_contracted_shop_name\\]');
-    ga('send', 'event', 'simulation-new-contract', 'btn-click', '', 0);
-  } else {
-    $('.can-hide').hide();
-    ga('send', 'event', 'simulation-change-contract', 'btn-click', '', 0);
-  }
-});
+  /**
+   * Scroll to...
+   */
+  $('h3 .scroll').on('click', function() {
+    scrollTo($('.scroll-info'), 0);
+  });
 
-/**
- * Scroll to...
- */
-$('h3 .scroll').on('click', function() {
-  scrollTo($('.scroll-info'), 0);
-});
+  $('.simulation-page-button').on('click', function() {
+    scrollTo($('.simulation-register-form'), 20);
+  });
 
-$('.simulation-page-button').on('click', function() {
-  scrollTo($('.simulation-register-form'), 20);
-});
+  /**
+   * Convert kanji to furigana
+   */
+  $.fn.autoKana('input[name=lpgas_contact\\[name\\]]', 'input[name=lpgas_contact\\[furigana\\]]');
 
-/**
- * Convert kanji to furigana
- */
-$.fn.autoKana('input[name=lpgas_contact\\[name\\]]', 'input[name=lpgas_contact\\[furigana\\]]');
-
-/*
- * SEND FORM
- */
-function sendForm(el) {
-  var form = el.closest('form');
-  var estimateKind = $('input[name=lpgas_contact\\[estimate_kind\\]]:checked').val();
-
-  var zip = $('input[name=zip]').val();
-  var pref = $('input[name=pref]').val();
-  var addr = $('input[name=addr]').val();
-
-  if (estimateKind == 'new_contract') {
-    var zipNewHidden = form.find('input[name=lpgas_contact\\[new_zip_code\\]]');
-    var prefNewHidden = form.find('input[name=lpgas_contact\\[new_prefecture_code\\]]');
-    var addrNewHidden = form.find('input[name=lpgas_contact\\[new_address\\]]');
-
-    zipNewHidden.val(zip);
-    prefNewHidden.val(pref);
-    addrNewHidden.val(addr);
-  } else {
-    var zipHidden = form.find('input[name=lpgas_contact\\[zip_code\\]]');
-    var prefHidden = form.find('input[name=lpgas_contact\\[prefecture_code\\]]');
-    var addrHidden = form.find('input[name=lpgas_contact\\[address\\]]');
-
-    zipHidden.val(zip);
-    prefHidden.val(pref);
-    addrHidden.val(addr);
+  // Show/Hide error on top of the form
+  function isError(is, el, msg) {
+    if (is) {
+      el.closest('form').find('.step-error').text(msg).addClass('show-error');
+    } else {
+      el.closest('form').find('.step-error').removeClass('show-error');
+    }
   }
 
-  $('#contact_btn p').text('送信中...');
-  if (sending_form !== true) {
-    sending_form = true;
-    ga('send', 'event', 'simulation-cv', 'btn-click', '', 0);
-    form.submit();
-  }
-}
+  function isInputValid(name) {
+    var input = $('input[name=' + name + ']').val();
 
-// Show/Hide error on top of the form
-function isError(is, el, msg) {
-  if (is) {
-    el.closest('form').find('.step-error').text(msg).addClass('show-error');
-  } else {
-    el.closest('form').find('.step-error').removeClass('show-error');
-  }
-}
+    if (input) {
+      inputHideError(name);
+    } else {
+      inputShowError(name);
+      return false;
+    }
 
-function isInputValid(name) {
-  var input = $('input[name=' + name + ']').val();
-
-  if (input) {
-    inputHideError(name);
-  } else {
-    inputShowError(name);
-    return false;
-  }
-
-  return true;
-}
-
-function isSelectValid(name) {
-  var select = $('#' + name).val();
-
-  if (select) {
-    $('#' + name).closest('.error-wrap').removeClass('input-error');
-  } else {
-    $('#' + name).closest('.error-wrap').addClass('input-error');
-    return false;
-  }
-
-  return true;
-}
-
-function isTelValid(name) {
-  var telInput = $('input[name=' + name + ']');
-  var tel = telInput.val();
-  var pattern = /^(\d{10,11})$/;
-
-  if (tel) {
-    tel = convertNumber(tel);
-    telInput.val(tel);
-  }
-
-  if (tel && tel.match(pattern)) {
-    inputHideError(name);
     return true;
   }
-  else {
-    inputShowError(name);
-    return false;
-  }
-}
 
-function isEmailValid(name) {
-  var mail = $('input[name=' + name + ']').val();
-  var pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?:[A-Z]{2}|com|org|net|gov|biz|info|jp)\b/;
+  function isSelectValid(name) {
+    var select = $('#' + name).val();
 
-  if (mail && mail.match(pattern)) {
-    inputHideError(name);
+    if (select) {
+      $('#' + name).closest('.error-wrap').removeClass('input-error');
+    } else {
+      $('#' + name).closest('.error-wrap').addClass('input-error');
+      return false;
+    }
+
     return true;
   }
-  else {
-    inputShowError(name);
-    return false;
+
+  function isTelValid(name) {
+    var telInput = $('input[name=' + name + ']');
+    var tel = telInput.val();
+    var pattern = /^(\d{10,11})$/;
+
+    if (tel) {
+      tel = convertNumber(tel);
+      telInput.val(tel);
+    }
+
+    if (tel && tel.match(pattern)) {
+      inputHideError(name);
+      return true;
+    }
+    else {
+      inputShowError(name);
+      return false;
+    }
   }
-}
 
-function inputShowError(name) {
-  $('input[name=' + name + ']').closest('.error-wrap').addClass('input-error');
-}
+  function isEmailValid(name) {
+    var mail = $('input[name=' + name + ']').val();
+    var pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?:[A-Z]{2}|com|org|net|gov|biz|info|jp)\b/;
 
-function inputHideError(name) {
-  $('input[name=' + name + ']').closest('.error-wrap').removeClass('input-error');
-}
+    if (mail && mail.match(pattern)) {
+      inputHideError(name);
+      return true;
+    }
+    else {
+      inputShowError(name);
+      return false;
+    }
+  }
 
-function convertNumber(str) {
-  return str.replace(/ー|－|−|-/g, '').replace(/[０-９]/g,function(s){return String.fromCharCode(s.charCodeAt(0)-0xFEE0);});
-}
+  function inputShowError(name) {
+    $('input[name=' + name + ']').closest('.error-wrap').addClass('input-error');
+  }
 
-function scrollTo(target, offset) {
-  var speed = 400;
-  var tg = target;
-  var position = tg.offset().top - offset;
+  function inputHideError(name) {
+    $('input[name=' + name + ']').closest('.error-wrap').removeClass('input-error');
+  }
 
-  $('body,html').animate({scrollTop:　position}, speed, 'swing');
+  function convertNumber(str) {
+    return str.replace(/ー|－|−|-/g, '').replace(/[０-９]/g,function(s){return String.fromCharCode(s.charCodeAt(0)-0xFEE0);});
+  }
+
+  function scrollTo(target, offset) {
+    var speed = 400;
+    var tg = target;
+    var position = tg.offset().top - offset;
+
+    $('body,html').animate({scrollTop:　position}, speed, 'swing');
+  }
 }
