6228469f53dac86c3c5f3b48ef80fd883e5f1023
Zazimko Alexey <notfoundsam@gmail.com>
2018-03-12 16:23:17 +0900

    not important

--- fuel/app/classes/controller/partner/estimates.php
+++ fuel/app/classes/controller/partner/estimates.php
@@ -122,12 +122,6 @@ class Controller_Partner_Estimates extends Controller_Partner
 
         if (\Input::extension() == 'pdf')
         {
-
-            // $this->template = View::forge('partner/estimates/show_pdf', [
-            //     'estimate' => $estimate,
-            // ], false);
-            // return;
-
             $options = new Options();
             $options->set('defaultFont', 'jgothic');
             $dompdf = new Dompdf($options);
