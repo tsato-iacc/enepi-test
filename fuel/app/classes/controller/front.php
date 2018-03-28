76a605bfe12441fefa9bf69cc7e49301d98a7207
Zazimko Alexey <notfoundsam@gmail.com>
2018-03-28 18:12:30 +0900

    fix kakaku

--- fuel/app/classes/controller/front.php
+++ fuel/app/classes/controller/front.php
@@ -115,6 +115,12 @@ class Controller_Front extends Controller_Template
         if ($media = $this->param('media'))
         {
             $this->estimate_post_url = "{$media}/lpgas/contacts";
+
+            if (\Input::method() == 'GET')
+            {
+                $tracking = new Tracking($this->param('media'));
+                $tracking->detect();
+            }
         }
 
         if (\Input::method() == 'GET' && \Request::active()->controller != 'Controller_Front_LpgasContacts')
