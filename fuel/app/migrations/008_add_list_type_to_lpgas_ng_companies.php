045b4f9eb4ad2e6dd50c0c8b2b93f8ab60229e4f
Zazimko Alexey <notfoundsam@gmail.com>
2018-05-10 16:10:36 +0900

    Add new column to lpgas_ng_companies and migration

---
+++ fuel/app/migrations/008_add_list_type_to_lpgas_ng_companies.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Fuel\Migrations;
+
+use \Fuel\Core\DB;
+
+class Add_list_type_to_lpgas_ng_companies
+{
+  public function up()
+  {
+    \DBUtil::add_fields('lpgas_ng_companies', [
+      'list_type' => [
+        'constraint' => '"black","white"',
+        'type' => 'enum',
+        'default' => 'black',
+        'after' => 'pattern',
+      ],
+    ]);
+  }
+
+  public function down()
+  {
+    \DBUtil::drop_fields('lpgas_ng_companies', [
+      'list_type',
+    ]);
+  }
+}
